2dbb9a708a9952d963bf32f8892113fa1e704de2
Your Name <you@example.com>
2022-12-05 21:22:11 +0100

    made some changes

--- Djangoproject/settings.py
+++ Djangoproject/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lhz0-=e6dt-shx*zfk&$nlrb!#5*o&ltig#$c#1q=)wtaqga^s'
+from portfolio.secret_settings import SECRET_KEY
+secret_key = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
